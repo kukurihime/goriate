f057b74b64b2dc69a6130ace13abb2788975a466
kukurihime <kukurihimeGit@gmail.com>
2023-02-05 23:55:06 +0900

    1st commit

---
+++ CGoriate.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 23:17:28 2023
+
+@author: kukurihime
+"""
+import time
+#import CTrainStatus
+import CGoriateControler
+#import CTrainCameraView
+#import CTrainCameraMQTTSub
+import CRepetationalThread
+
+
+class CGoriate(CRepetationalThread.CRepetationalThread):
+    def __init__(self, argv, interval = 0.1):
+        super().__init__(interval)
+#        self.ts = CTrainStatus.CTrainStatus(argv)
+#        self.tcv = CTrainCameraView.CTrainCameraView(self.ts) #another thread
+#        self.gc = CGoriateControler.CGoriateControler(self.ts)
+        self.gc = CGoriateControler.CGoriateControler()
+#        self.tcms = CTrainCameraMQTTSub.CTrainCameraMQTTSub(self.tc)
+        
+#        self.tcv.start()
+        
+    def func(self):
+        #if not self.tcms.isConnected():
+        #    print('test2')
+        #    self.tcms.connect()
+        
+#        self.valueUpdate()
+#        self.targetUpdate()
+        self.execute()
+#       if not self.ts.getCommand() == 'q':
+#           self.setCommand( "")
+    
+#   def valueUpdate(self):
+#       self.ts.update()
+    
+#    def targetUpdate(self):
+#        self.tc.statusUpdate()
+
+    def execute(self): 
+        self.gc.run()
+        
+#    def setCommand(self, command):
+#        self.command = command
+#        self.tc.setCommand(command)
+        
+#    def getCommand(self):
+#        return self.ts.getCommand()
+    
+#    def finish(self):
+#        self.tcv.join()
+    
+   
+    def demo(self):
+        time.sleep(1)
+        test = [0.5, 0.5]
+        self.gc.PWMOutputPair(test)
+        time.sleep(5)
+        
+        self.gc.stopPWMPair()
---
+++ goriate.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 22:53:54 2023
+
+@author: kukurihime
+"""
+
+
+import CGoriate
+#import CRealtimeKeyInput
+import time
+import sys
+
+
+print("goriate Start!!")
+time.sleep(0.5)
+
+argv = sys.argv
+#if len( argv ) == 1:
+#    argv.append("real") 
+
+go = CGoriate.CGoriate(argv)
+#go.start() #other thread
+go.demo()
+
+#command proccess loop
+#rki = CRealtimeKeyInput.CRealtimeKeyInput()
+#rki.setDaemon(True)
+#rki.start()
+#key = ''
+#while not key == 'q':
+#    if rki.hasNewKey():
+#        key = rki.getKey()
+#        cg.setCommand(key)
+#    else:
+#        key = cg.getCommand()
+    #print("key:", key)
+#    if key == 'q':
+#        rki.stop()
+#        pass
+#        
+#    time.sleep(0.1)
+    
+        
+
+print( "quit command")
+#rki.finish()
+#go.finish()
+#go.join()
+
+print("goriate Finished!!")
+
+
+
