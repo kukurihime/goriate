de29e9f57b0455237f3ec2f87d2ce5e8b088181e
kukurihime <kukurihimeGit@gmail.com>
2023-02-06 00:22:05 +0900

    7th test

--- CRPiControler.py
+++ CRPiControler.py
@@ -17,7 +17,7 @@ class CRPiControler:
             self.H = 1
             self.L = 0
                 
-            self.freq = 500 #PWMFrequency
+            self.freq = 200 #PWMFrequency
             self.raspberryPiPWMValue = 1000000 #raspberryPi PWM
             
         def ready(self):
