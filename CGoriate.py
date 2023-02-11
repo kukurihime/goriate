66d49e96d0609a9f4d0e5f3065bd4d38414d81f4
kukurihime <kukurihimeGit@gmail.com>
2023-02-11 20:16:16 +0900

    8th test

--- CGoriate.py
+++ CGoriate.py
@@ -57,7 +57,7 @@ class CGoriate(CRepetationalThread.CRepetationalThread):
    
     def demo(self):
         time.sleep(1)
-        test = [0.7, 0.7]
+        test = [0.8, 0.8]
         self.gc.outputPWMPair(test)
         time.sleep(5)
         
