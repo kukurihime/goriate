40d982618176e09c7bf706dff600558c1523e599
kukurihime <kukurihimeGit@gmail.com>
2023-10-22 16:09:22 +0900

    goriate update

--- CRPiControler.py
+++ CRPiControler.py
@@ -17,7 +17,7 @@ class CRPiControler:
             self.H = 1
             self.L = 0
                 
-            self.freq = 50000 #PWMFrequency
+            self.freq = 500000 #PWMFrequency
             self.raspberryPiPWMValue = 1000000 #raspberryPi PWM
             
         def ready(self):
