bb4228db65b54df85011c7fc064896a310f9b180
kukurihime <kukurihimeGit@gmail.com>
2023-10-22 15:10:27 +0900

    goriate update

--- CRPiControler.py
+++ CRPiControler.py
@@ -9,29 +9,29 @@ class CRPiControler:
             if not self.dummyFlg:
                 self.pi = pigpio.pi()
                 
-#            self.vccP = 26 #VccIn
+            self.vccP = 26 #VccIn
             self.in1P = 12 #hardwarePWM0
             self.in2P = 13 #hardwarePWM1
-#            self.ledP = 21 #LED
+            self.ledP = 21 #LED
 
             self.H = 1
             self.L = 0
                 
-            self.freq = 500000 #PWMFrequency
+            self.freq = 1000 #PWMFrequency
             self.raspberryPiPWMValue = 1000000 #raspberryPi PWM
             
         def ready(self):
             if self.dummyFlg:
                 return
-#            self.pi.set_mode(self.vccP, pigpio.OUTPUT)
+            self.pi.set_mode(self.vccP, pigpio.OUTPUT)
             self.pi.set_mode(self.in1P, pigpio.ALT0)
             self.pi.set_mode(self.in2P, pigpio.ALT0)
-#            self.pi.set_mode(self.ledP, pigpio.OUTPUT)
+            self.pi.set_mode(self.ledP, pigpio.OUTPUT)
 
-#            self.on(self.vccP)
+            self.on(self.vccP)
             self.off(self.in1P)
             self.off(self.in2P)
-#            self.off(self.ledP)
+            self.off(self.ledP)
             
         def finish(self):
             if self.dummyFlg:
@@ -40,23 +40,16 @@ class CRPiControler:
             self.PWMStop(self.in1P)
             self.PWMStop(self.in2P)
             time.sleep(1)
-#            self.off(self.vccP)
-#            self.off(self.ledP)
+            self.off(self.vccP)
+            self.off(self.ledP)
         
             self.pi.set_mode(self.in1P, pigpio.INPUT)
             self.pi.set_mode(self.in2P, pigpio.INPUT)
-#            self.pi.set_mode(self.vccP, pigpio.INPUT)
-#            self.pi.set_mode(self.ledP, pigpio.INPUT)
+            self.pi.set_mode(self.vccP, pigpio.INPUT)
+            self.pi.set_mode(self.ledP, pigpio.INPUT)
             self.pi.stop()
 
         def PWMDuty(self, dutyRatio):
-            if dutyRatio < 0:
-                dutyRatio = 0
-            elif dutyRatio > 1:
-                dutyRatio = 1
-            else:
-                pass
-            
             return int(self.raspberryPiPWMValue * dutyRatio)
         
         def PWMOutput(self, pin, dutyRatio):
