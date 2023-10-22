f1f446e9505efff555ca6dc1f90f2c833f39b092
kukurihime <kukurihimeGit@gmail.com>
2023-10-22 16:00:23 +0900

    goriate update

--- CGoriateControler.py
+++ CGoriateControler.py
@@ -60,7 +60,7 @@ class CGoriateControler:
         self.rpc.pwmOutput(self.rpc.leftPWM, 0.9)
         
         self.rpc.off(self.rpc.rightD)
-        self.rpc.pwmOutput(self.rpc.rightPWM)
+        self.rpc.pwmOutput(self.rpc.rightPWM, 0.9)
         
     def stopPWMPair(self):
         self.rpc.pwmStop(12)
